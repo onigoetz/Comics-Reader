2260d638d7cea828d35bd92ab41dd70bc587a88b
Stéphane Goetz <onigoetz@onigoetz.ch>
2018-12-03 20:04:27 +0100

    Clarify read/unread status in lists. Update dependencies

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -1,16 +1,29 @@
 import React from "react";
 import LazyLoad from "react-lazyload";
 import { Link } from "react-router-dom";
+import classnames from "classnames";
 
 import { thumb, createUrl } from "../utils";
 import { TYPE_DIR } from "../types";
+
+function allRead(folder) {
+  return folder.type === TYPE_DIR && folder.booksInside === folder.booksInsideRead;
+}
+
+function unreadBook(folder) {
+  return folder.type !== TYPE_DIR && !folder.read;
+}
+
+function cleanName(name) {
+  return name.replace(/(\.(:?cbr|cbz|zip|rar|pdf))$/, "");
+}
 
 function getRead(folder) {
   if (folder.type !== TYPE_DIR || !folder.booksInsideRead) {
     return null;
   }
 
-  if (folder.booksInside === folder.booksInsideRead) {
+  if (allRead(folder)) {
     return <small>(All Read)</small>;
   }
 
@@ -26,17 +39,12 @@ export default function List({ books }) {
         }
 
         return (
-          <li className="List__cell" key={folder.name}>
+          <li className={classnames("List__cell", {"List__cell--allRead": allRead(folder)})} key={folder.name}>
             <Link to={createUrl(folder)}>
               <LazyLoad height={140} offset={200}>
                 <img src={thumb(folder.path)} height="140" alt={folder.name} />
               </LazyLoad>
-              <span className="List__cell__name">{folder.name}</span>
-              {folder.read && (
-                <p>
-                  <small>Read</small>
-                </p>
-              )}
+              <div className="List__cell__name">{unreadBook(folder) && <span className="UnreadBadge" />}{cleanName(folder.name)}</div>
               {folder.type === TYPE_DIR && (
                 <p>
                   {folder.booksInside} Books {getRead(folder)}
