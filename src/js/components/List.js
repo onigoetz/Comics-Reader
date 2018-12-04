2109dcd2146fc72928178b587053f459017fd836
Stéphane Goetz <onigoetz@onigoetz.ch>
2018-12-04 10:32:28 +0100

    Tweak read/unread for books and folders

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -10,8 +10,12 @@ function allRead(folder) {
   return folder.type === TYPE_DIR && folder.booksInside === folder.booksInsideRead;
 }
 
-function unreadBook(folder) {
-  return folder.type !== TYPE_DIR && !folder.read;
+function unread(folder) {
+  if (folder.type === TYPE_DIR) {
+    return folder.booksInside !== folder.booksInsideRead;
+  }
+
+  return !folder.read;
 }
 
 function cleanName(name) {
@@ -33,27 +37,23 @@ function getRead(folder) {
 export default function List({ books }) {
   return (
     <ul className="List">
-      {books.map(folder => {
-        if (!folder) {
-          return <li>Not Found</li>;
-        }
-
-        return (
-          <li className={classnames("List__cell", {"List__cell--allRead": allRead(folder)})} key={folder.name}>
-            <Link to={createUrl(folder)}>
+      {books.filter(Boolean).map(folder => (
+        <li className={classnames("List__cell", {"List__cell--allRead": allRead(folder), "List__cell--unread": unread(folder)})} key={folder.name}>
+          <Link to={createUrl(folder)}>
+            <div className="List__cell__image">
               <LazyLoad height={140} offset={200}>
                 <img src={thumb(folder.path)} height="140" alt={folder.name} />
               </LazyLoad>
-              <div className="List__cell__name">{unreadBook(folder) && <span className="UnreadBadge" />}{cleanName(folder.name)}</div>
-              {folder.type === TYPE_DIR && (
-                <p>
-                  {folder.booksInside} Books {getRead(folder)}
-                </p>
-              )}
-            </Link>
-          </li>
-        );
-      })}
+            </div>
+            <div className="List__cell__name">{cleanName(folder.name)}</div>
+            {folder.type === TYPE_DIR && (
+              <p className="List__cell__details">
+                {folder.booksInside} Books {getRead(folder)}
+              </p>
+            )}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
