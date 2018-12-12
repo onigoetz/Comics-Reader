da245734f89638b284242ab3019d473e72b5d800
Stéphane Goetz <onigoetz@onigoetz.ch>
2018-12-12 22:46:16 +0100

    More improvements on unread visualization

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -11,45 +11,42 @@ function allRead(folder) {
 }
 
 function unread(folder) {
-  if (folder.type === TYPE_DIR) {
-    return folder.booksInside !== folder.booksInsideRead;
-  }
-
-  return !folder.read;
+  return folder.type !== TYPE_DIR && !folder.read;
 }
 
-function getRead(folder) {
-  if (folder.type !== TYPE_DIR || !folder.booksInsideRead) {
-    return null;
-  }
-
-  if (allRead(folder)) {
-    return <small>(All Read)</small>;
-  }
-
-  return <small>({folder.booksInsideRead} Read)</small>;
+function bookCount(folder) {
+  return folder.booksInside === 1 ? "1 book" : `${folder.booksInside} books`;
 }
 
 export default function List({ books }) {
   return (
     <ul className="List">
-      {books.filter(Boolean).map(folder => (
-        <li className={classnames("List__cell", {"List__cell--allRead": allRead(folder), "List__cell--unread": unread(folder)})} key={folder.name}>
-          <Link to={createUrl(folder)}>
-            <div className="List__cell__image">
-              <LazyLoad height={140} offset={200}>
-                <img src={thumb(folder.path)} height="140" alt={folder.name} />
-              </LazyLoad>
-            </div>
-            <div className="List__cell__name">{cleanName(folder.name)}</div>
-            {folder.type === TYPE_DIR && (
-              <p className="List__cell__details">
-                {folder.booksInside} Books {getRead(folder)}
-              </p>
-            )}
-          </Link>
-        </li>
-      ))}
+      {books.filter(Boolean).map((folder) => {
+        const classes = {
+          "List__cell--allRead": allRead(folder),
+          "List__cell--unread": unread(folder)
+        };
+        return (
+          <li className={classnames("List__cell", classes)} key={folder.name}>
+            <Link to={createUrl(folder)}>
+              <div className="List__cell__image">
+                {folder.type === TYPE_DIR && folder.booksInside !== folder.booksInsideRead && (
+                  <div className="List__cell__unreadCount">{folder.booksInside - folder.booksInsideRead}</div>
+                )}
+                <LazyLoad height={140} offset={200}>
+                  <img src={thumb(folder.path)} height="140" alt={folder.name} />
+                </LazyLoad>
+              </div>
+              <div className="List__cell__name">{cleanName(folder.name)}</div>
+              {folder.type === TYPE_DIR && (
+                <p className="List__cell__details">
+                  {bookCount(folder)}
+                </p>
+              )}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
