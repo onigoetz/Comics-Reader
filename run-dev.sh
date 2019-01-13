ff8b27f3446c370fd3c7cc2aa337f842ce06f9ea
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-01-13 22:39:48 +0100

    Add nodemon to reload on change

--- run-dev.sh
+++ run-dev.sh
@@ -1,2 +1,7 @@
 docker build -t comics-reader .
-docker run -it --rm -v "$PWD":/var/www/html/ -v "$PWD/images":/comics/ -p 8080:8080 comics-reader
+docker run -it --rm \
+    -v "$PWD":/var/www/html/ \
+    -v "$PWD/images":/comics/ \
+    -p 8080:8080 \
+    comics-reader \
+    yarn start:dev
