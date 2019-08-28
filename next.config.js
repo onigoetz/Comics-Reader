6f3ff7ffc370552ce40e7c6eb0f678472d5847cb
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-08-28 23:05:35 +0200

    Fix configuration

--- next.config.js
+++ next.config.js
@@ -47,6 +47,6 @@ const nextConfiguration = {
   }
 };
 
-const plugins = [withBundleAnalyzer({}), withCSS];
+const plugins = [withBundleAnalyzer, withCSS];
 
 module.exports = withPlugins([...plugins], nextConfiguration);
