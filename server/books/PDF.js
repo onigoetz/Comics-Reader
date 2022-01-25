d72653d8ebb9e28ff9db86fcff733097e483dd73
Stéphane Goetz <onigoetz@onigoetz.ch>
2022-01-25 22:55:58 +0100

    Remove the image pdf extractor as it's slower than mutool draw

--- server/books/PDF.js
+++ server/books/PDF.js
@@ -8,7 +8,6 @@ const pdfjs = require("pdfjs-dist/legacy/build/pdf.js");
 const { exec, createTempSymlink } = require("../exec");
 const { getBigatureSize } = require("../utils");
 
-
 module.exports = class PDF {
   constructor(file) {
     this.file = file;
@@ -27,68 +26,7 @@ module.exports = class PDF {
     return pdfjs.getDocument({ data }).promise;
   }
 
-  async extractPageWithLib(pageNum) {
-    const doc = await this.getDocument();
-
-    const page = await doc.getPage(pageNum);
-    const ops = await page.getOperatorList();
-    const images = [];
-
-    for (let j = 0; j < ops.fnArray.length; j++) {
-      if (
-        ops.fnArray[j] === pdfjs.OPS.paintJpegXObject ||
-        ops.fnArray[j] === pdfjs.OPS.paintImageXObject
-      ) {
-        images.push({
-          type: ops.fnArray[j],
-          obj: page.objs.get(ops.argsArray[j][0])
-        });
-      }
-    }
-
-    if (images.length !== 1) {
-      throw new Error(
-        "Could not find a JPG image on this page or find too many"
-      );
-    }
-
-    var svgGfx = new pdfjs.SVGGraphics(page.commonObjs, page.objs);
-
-    const img = images[0];
-
-    let renderedImage;
-    switch (img.type) {
-      case pdfjs.OPS.paintImageXObject:
-        svgGfx.paintInlineImageXObject(img.obj, {
-          appendChild: el => {
-            renderedImage = el.attributes["xlink:href"].substring(22);
-          }
-        });
-        break;
-      case pdfjs.OPS.paintJpegXObject:
-        renderedImage = img.obj.src.substring(23);
-        break;
-    }
-
-    // It would be better to keep it as a buffer
-    // But libraries like image-size can't deal with it
-    const file = await tmp.file({ postfix: ".png" });
-    await fs.promises.writeFile(
-      file.path,
-      Buffer.from(renderedImage, "base64")
-    );
-
-    return file;
-  }
-
   async extractPage(pageNum) {
-    try {
-      // We must await here otherwise the error will bubble up
-      return await this.extractPageWithLib(pageNum);
-    } catch (e) {
-      console.error(`Could not extract file ${e.message}`);
-    }
-
     const file = await tmp.file({ postfix: ".png" });
 
     const { filePath, cleanup } = await createTempSymlink(this.file);
