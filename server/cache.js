30307a0eaa047aed4f54ff0b2b620d97b4fb4405
Stéphane Goetz <onigoetz@onigoetz.ch>
2022-10-29 15:59:23 +0200

    Update dependencies

--- server/cache.js
+++ server/cache.js
@@ -1,11 +1,18 @@
 const cacheManager = require("cache-manager");
 const fsStore = require("cache-manager-fs-hash");
 
-module.exports = cacheManager.caching({
-  store: fsStore,
-  options: {
+const cachePreparation = cacheManager.caching(
+  fsStore.create({
     ttl: 60 * 60 * 24 * 90, // 3 months lifetime (in seconds)
     path: process.env.DIR_CACHE,
     subdirs: true
+  })
+);
+
+module.exports = {
+  async wrap(key, fn, options) {
+    const cache = await cachePreparation;
+
+    return cache.wrap(key, fn, options);
   }
-});
+}
